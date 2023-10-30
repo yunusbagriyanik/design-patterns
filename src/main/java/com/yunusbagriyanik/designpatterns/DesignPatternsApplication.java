9a7e54cd95c62cba2104a9e60a8f92ef95447cb8
Yunus Bagriyanik <yunusbgrynk@gmail.com>
2023-10-31 02:39:07 +0300

    app main class

--- src/main/java/com/yunusbagriyanik/designpatterns/DesignPatternsApplication.java
+++ src/main/java/com/yunusbagriyanik/designpatterns/DesignPatternsApplication.java
@@ -1,5 +1,8 @@
 package com.yunusbagriyanik.designpatterns;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class DesignPatternsApplication {
 	public static void main(String[] args) {
 
