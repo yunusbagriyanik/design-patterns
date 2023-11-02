80067f671b617c065dbd5427e7f127cfe26c7e83
Yunus Bagriyanik <yunusbgrynk@gmail.com>
2023-11-02 03:09:43 +0300

    compressed data source decorator

--- src/main/java/com/yunusbagriyanik/designpatterns/decorator/example2/main/DecoratorStarter.java
+++ src/main/java/com/yunusbagriyanik/designpatterns/decorator/example2/main/DecoratorStarter.java
@@ -1,5 +1,6 @@
 package com.yunusbagriyanik.designpatterns.decorator.example2.main;
 
+import com.yunusbagriyanik.designpatterns.decorator.example2.design.compression.CompressedDataSourceDecorator;
 import com.yunusbagriyanik.designpatterns.decorator.example2.design.datasource.DataSource;
 import com.yunusbagriyanik.designpatterns.decorator.example2.design.datasource.DataSourceDecorator;
 import com.yunusbagriyanik.designpatterns.decorator.example2.design.datasource.FileDataSource;
@@ -11,11 +12,18 @@ public class DecoratorStarter {
     private static final String FILE_NAME = "src/main/resources/decorator.txt";
 
     public static void main(String[] args) {
+        DataSource plain = new FileDataSource(FILE_NAME);
+
         String input = "Hello World!";
         DataSourceDecorator encoded = new EncryptionDecorator(new FileDataSource(FILE_NAME));
         encoded.write(input);
-        DataSource plain = new FileDataSource(FILE_NAME);
         log.info("\nEncoded\n{}", plain.read());
         log.info("\nDecoded\n{}", encoded.read());
+
+        System.out.println("**");
+        DataSourceDecorator encodedCompDecorator = new CompressedDataSourceDecorator(new EncryptionDecorator(new FileDataSource(FILE_NAME)));
+        encodedCompDecorator.write(input);
+        log.info("\nEncoded\n{}", plain.read());
+        log.info("\nDecoded\n{}", encodedCompDecorator.read());
     }
 }
