fe79a4769185f19c55f445b2ee9ca3d07938055f
Yunus Bagriyanik <yunusbgrynk@gmail.com>
2025-02-25 23:30:53 +0300

    builder pattern

---
+++ src/main/java/com/yunusbagriyanik/designpatterns/cretional/builder/BuilderMain.java
@@ -0,0 +1,13 @@
+package com.yunusbagriyanik.designpatterns.cretional.builder;
+
+public class BuilderMain {
+    public static void main(String[] args) {
+        HTMLBuilder htmlBuilder = new HTMLBuilder();
+        String html = htmlBuilder.addTitle("Builder Pattern")
+                .addParagraph("Builder Pattern - Creational Category")
+                .addParagraph("The builder pattern separates the construction of a complex object from its representation.")
+                .addParagraph("Source: https://en.wikipedia.org/wiki/Builder_pattern")
+                .build();
+        System.out.println(html);
+    }
+}
---
+++ src/main/java/com/yunusbagriyanik/designpatterns/cretional/builder/HTMLBuilder.java
@@ -0,0 +1,23 @@
+package com.yunusbagriyanik.designpatterns.cretional.builder;
+
+public class HTMLBuilder {
+    private final StringBuilder html;
+
+    public HTMLBuilder() {
+        html = new StringBuilder();
+    }
+
+    public HTMLBuilder addTitle(String title) {
+        html.append("<h1>").append(title).append("</h1>");
+        return this;
+    }
+
+    public HTMLBuilder addParagraph(String text) {
+        html.append("<p>").append(text).append("</p>");
+        return this;
+    }
+
+    public String build() {
+        return html.toString();
+    }
+}
